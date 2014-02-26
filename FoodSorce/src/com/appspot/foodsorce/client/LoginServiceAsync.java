7b57e78d706a69d6aa8d1431a12591b562eee093
Norman Sue <normful@gmail.com>
2014-02-26 11:44:44 -0800

    add initial login functionality

---
+++ FoodSorce/src/com/appspot/foodsorce/client/LoginServiceAsync.java
@@ -0,0 +1,7 @@
+package com.appspot.foodsorce.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+public interface LoginServiceAsync {
+	public void login(String requestUri, AsyncCallback<LoginInfo> async);
+}
---
+++ FoodSorce/src/com/appspot/foodsorce/client/NotLoggedInException.java
@@ -0,0 +1,13 @@
+package com.appspot.foodsorce.client;
+
+import java.io.Serializable;
+
+public class NotLoggedInException extends Exception implements Serializable {
+	public NotLoggedInException() {
+		super();
+	}
+
+	public NotLoggedInException(String message) {
+		super(message);
+	}
+}
