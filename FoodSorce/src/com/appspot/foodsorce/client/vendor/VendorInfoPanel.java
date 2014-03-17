92ef8390a3c13f5e7edd49f2f568f595b9a13972
Norman Sue <normful@gmail.com>
2014-03-17 08:31:26 -0700

    add ViewReviewsPanel stub

--- FoodSorce/src/com/appspot/foodsorce/client/vendor/VendorInfoPanel.java
+++ FoodSorce/src/com/appspot/foodsorce/client/vendor/VendorInfoPanel.java
@@ -1,6 +1,7 @@
 package com.appspot.foodsorce.client.vendor;
 
 import com.appspot.foodsorce.shared.Vendor;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.ScrollPanel;
@@ -13,17 +14,28 @@ public class VendorInfoPanel extends VerticalPanel {
 	private Label vendorName;
 	private Label vendorLocation;
 	private Label vendorDescription;
+	private Button addReviewButton;
+	private ViewReviewsPanel viewReviewsPanel;
 	
 	public VendorInfoPanel(Vendor vendor) {
 		vendorName = new Label(vendor.getName());
-		vendorDescription = new Label(vendor.getDescription());
-		vendorLocation = new Label(vendor.getLocation());
 		vendorName.setStylePrimaryName("vendorInfoPanelName");
+		
+		vendorDescription = new Label(vendor.getDescription());
 		vendorDescription.setStylePrimaryName("vendorInfoPanelDescription");
+		
+		vendorLocation = new Label(vendor.getLocation());
 		vendorLocation.setStylePrimaryName("vendorInfoPanelLocation");
+		
+		addReviewButton = new Button("Add Review");
+		viewReviewsPanel = new ViewReviewsPanel(vendor);
+		
 		htmlPanel.add(vendorName);
 		htmlPanel.add(vendorDescription);
 		htmlPanel.add(vendorLocation);
+		htmlPanel.add(addReviewButton);
+		htmlPanel.add(viewReviewsPanel);
+		
 		scrollPanel.add(htmlPanel);
 		this.add(scrollPanel);
 	}
---
+++ FoodSorce/src/com/appspot/foodsorce/client/vendor/ViewReviewsPanel.java
@@ -0,0 +1,13 @@
+package com.appspot.foodsorce.client.vendor;
+
+import com.appspot.foodsorce.shared.Vendor;
+import com.google.gwt.user.client.ui.VerticalPanel;
+
+public class ViewReviewsPanel extends VerticalPanel {
+
+	Vendor vendor;
+	
+	public ViewReviewsPanel(Vendor vendor) {
+		this.vendor = vendor;
+	}
+}
